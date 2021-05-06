7838baea361b3e79d66018842142fdd236963910
Dianajarenga <79199013+Dianajarenga@users.noreply.github.com>
2021-05-06 06:58:12 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+a = {6,7,8,9,10}
+b = {5, 6, 7, 8, 9}
+for i in a:
+        a.insert(1,3)
+print(b.insert(1,4))
+print(a.union(b))
+print(b.union(a))
+print(a.difference(b))
+
+lst = [11, 100, 99, 1000, 999, 99]
+print(lst.replace([-1],100))
+print(lst[1])
+print(lst[5])
+
+for i in lst:
+      sum+=i
+      print(sum)
+for i in lst:
+    if i<99:
+        print(i)
+
+ range
+range(2020,2070)
+for i in  range(1000 ,2000 ):
+                      if i%7==0 & i%5==0:
+                                   print(i)
+
+
+
+
+
