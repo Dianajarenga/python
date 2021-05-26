6492ec37fcfe075a865d3946fafc4cdaa84bc737
Dianajarenga <79199013+Dianajarenga@users.noreply.github.com>
2021-05-26 20:26:58 +0300

    Add files via upload

--- madlibs.py
+++ madlibs.py
@@ -23,22 +23,22 @@ def student_form():
     id=(input("Id: "))
     unit=(input("Unit of study: "))
     year=(input("Year: "))
-    record=name1,name2,age,id,unit,year,institution
+    record=institution,name1,name2,age,id,unit,year
     print( f" Hello {name1} {name2}, your age {age} and Id {id} confirm that you are eligible to join {institution},\n to study {unit} in {year}"  + "\n"
        + str(record))
     return record
 student_form()
 
 def flight_booking():
-    Airport=(input("AirPort :"))
-    Social_class=(input("Social Class :"))
-    Price=int(input("Price :"))
-    Payment_method=(input("Payment Method"))
-    Date=(input("Date :"))
+    airport=(input("AirPort :"))
+    social_class=(input("Social Class :"))
+    price=int(input("Price :"))
+    payment_method=(input("Payment Method"))
+    date=(input("Date :"))
     seat=(input("Seat :"))
-    Expiry="23/09/2021"
-    Approved:bool=True
-    ticket=Airport,Approved,Social_class,Price,Payment_method,Date,Approved,seat,Expiry
-    print(f"Hello,{name},welcome to{Airport},you have booked for{Social_class},at Ksh{Price},through{Payment_method} is {Approved} \n your seat number is {seat},your flight is on{date},expiry{expiry}")
+    expiry="23/09/2021"
+    approved:bool=True
+    ticket=airport,approved,social_class,price,payment_method,date,approved,seat,expiry
+    print(f"Hello,{name},welcome to{airport},you have booked for{social_class},at Ksh{price},through{payment_method} is {approved} \n  your seat number is {seat},your flight is on{date},expiry{expiry}")
     return ticket
 flight_booking()
